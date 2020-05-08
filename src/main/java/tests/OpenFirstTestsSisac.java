d9d69e8d508d50cbc23285d89d8145ce7a58af9d
Rafael Ferreira <raffa.ferreira@hotmail.com>
2020-05-08 09:49:31 -0300

    Atualizando jsonHandler

---
+++ src/main/java/tests/OpenFirstTestsSisac.java
@@ -0,0 +1,77 @@
+package tests;
+
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import managers.WebDriverManager;
+import page_objects.LogInBank;
+import utilities.JsonHandler;
+
+public class OpenFirstTestsSisac extends WebDriverManager {
+
+	OpenFirstTestsSisac() throws Exception {
+
+	}
+
+	JsonHandler jsonHandler = new JsonHandler();
+	JSONObject jsonObject = null;
+	String username;
+	String password;
+	
+
+	@org.testng.annotations.BeforeClass
+	public void BeforeClass() throws IOException, ParseException {
+
+		jsonObject = jsonHandler.getDataFile("UsersData.json");
+
+	}
+
+	@Test
+	public void verifyAcessSuccessfully() throws IOException, ParseException {
+
+		jsonObject = jsonHandler.getDataGroup(jsonObject, "valid");
+		username = (String) jsonObject.get("username");
+		password = (String) jsonObject.get("password");
+		new LogInBank(driver).loginBank(username, password);
+
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Digital Bank");
+	}
+	
+	@Test
+	public void verifyAcessInvalid() throws IOException, ParseException {
+
+		
+		jsonObject = jsonHandler.getDataGroup(jsonObject, "invalid");
+		username = (String) jsonObject.get("username");
+		password = (String) jsonObject.get("password");
+		new LogInBank(driver).loginBank(username, password);
+
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Digital Bank");
+	}
+	
+	
+	@Test
+	public void verifyAcessEmpty() throws IOException, ParseException {
+
+		
+		jsonObject = jsonHandler.getDataGroup(jsonObject, "allEmpty");
+		username = (String) jsonObject.get("username");
+		password = (String) jsonObject.get("password");
+		new LogInBank(driver).loginBank(username, password);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Digital Bank");
+	}
+
+
+
+	@AfterClass
+	public void AfterMethod() {
+		WebDriverManager.CloseDriver();
+	}
+
+}
--- src/main/java/utilities/JsonHandler.java
+++ src/main/java/utilities/JsonHandler.java
@@ -10,10 +10,8 @@ import org.json.simple.parser.ParseException;
 
 public class JsonHandler {
 
-	public  JSONObject getDataFile(String fileName)
-			throws IOException, ParseException {
-		
-		
+	public JSONObject getDataFile(String fileName) throws IOException, ParseException {
+
 		InputStream inputStream = getClass().getResourceAsStream("/" + fileName);
 		BufferedReader buffRead = new BufferedReader(new InputStreamReader(inputStream));
 
@@ -39,37 +37,12 @@ public class JsonHandler {
 		return jsonDataObject;
 
 	}
-	
-	public JSONObject getDataGroup (JSONObject jsonDataObject,String dataGroup) {
-		
+
+	public JSONObject getDataGroup(JSONObject jsonDataObject, String dataGroup) {
+
 		// retornando o objeto json, filtrando pelo grupo de dados desejado
 		return (JSONObject) jsonDataObject.get(dataGroup);
-		
+
 	}
-	
 
 }
-
-/*
- * //criando método que recebe o nome do arquivo json e o nome do grupo de dados
- * desejado protected JSONObject getJsonDataObject(String jsonFileName, String
- * dataGroup) {
- * 
- * // criando um parses para utilizamos no momento da leitura do arquivo json
- * JSONParser parser = new JSONParser();
- * 
- * // criando um objeto json que vai receber os dados do arquivo json JSONObject
- * jsonDataObject = null; try { // inserindo o arquivo json em um objeto Object
- * jsonFileObject = parser.parse(new FileReader(System.getProperty("user.dir") +
- * File.separator + "src" + File.separator + "main" + File.separator + "java" +
- * File.separator + "data" + File.separator + jsonFileName));
- * 
- * // convertendo o objeto com o arquivo json para o formato de objeto json
- * jsonDataObject = (JSONObject) jsonFileObject; } catch (IOException |
- * ParseException e) { e.printStackTrace(); }
- * 
- * // garantindo que o objeto json não está nulo assert jsonDataObject != null;
- * 
- * // retornando o objeto json, filtrando pelo grupo de dados desejado return
- * (JSONObject) jsonDataObject.get(dataGroup);
- */
